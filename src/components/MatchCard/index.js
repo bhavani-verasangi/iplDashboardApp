4b3b5bbebd102b4c9f4b9e97299199d2b4f2ea9b
bhavani-verasangi <54885240+bhavani-verasangi@users.noreply.github.com>
2023-07-15 12:58:56 +0530

    Update index.js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,25 +1,26 @@
-// Write your code here
 import './index.css'
 
-import {Component} from 'react'
+const MatchCard = props => {
+  const {matchDetails} = props
+  const {competingTeamLogo, competingTeam, matchStatus, result} = matchDetails
+  const getMatchStatusClassName = status =>
+    status === 'Won' ? 'match-won' : 'match-lost'
+  const matchStatusClassName = `match-status ${getMatchStatusClassName(
+    matchStatus,
+  )}`
 
-class MatchCard extends Component {
-  render() {
-    const {matchDetails} = this.props
-    const {competingTeamLogo, competingTeam, matchStatus, result} = matchDetails
-    return (
-      <li className={`match-card ${matchStatus}`}>
-        <img
-          src={competingTeamLogo}
-          className="competing-team-logo"
-          alt={`competing team ${competingTeam}`}
-        />
-        <p className="competing-team-name">{competingTeam}</p>
-        <p className="result">{result}</p>
-        <p className="match-status">{matchStatus}</p>
-      </li>
-    )
-  }
+  return (
+    <li className="match-item">
+      <img
+        src={competingTeamLogo}
+        className="competing-team-logo"
+        alt={`competing team ${competingTeam}`}
+      />
+      <p className="competing-team-name">{competingTeam}</p>
+      <p className="result">{result}</p>
+      <p className={matchStatusClassName}>{matchStatus}</p>
+    </li>
+  )
 }
 
 export default MatchCard
