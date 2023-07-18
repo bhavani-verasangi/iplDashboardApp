227c4541ad892a9dbdd49537767d58b564700e11
bhavani-verasangi <54885240+bhavani-verasangi@users.noreply.github.com>
2023-07-18 13:20:23 +0530

    Update index.js

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -76,7 +76,7 @@ class TeamMatches extends Component {
   }
 
   renderLoader = () => (
-    <div data-testid="loader" className="loader-container">
+    <div testid="loader" className="loader-container">
       <Loader type="Oval" color="#ffffff" height={50} />
     </div>
   )
